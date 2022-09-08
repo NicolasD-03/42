c3f90beed775fd8509fe2a7a326b0ebc631b79bf
Nicolas Dick <ndick@k1r1p12.42mulhouse.fr>
2022-09-08 18:41:57 +0200

    Feat: C04 done ready for correction

---
+++ C04/ex05/ft_atoi_base.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndick <ndick@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 16:32:30 by ndick             #+#    #+#             */
+/*   Updated: 2022/09/08 18:20:53 by ndick            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	check_base(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if ((base[i] >= 9 && base[i] <= 13) || base[i] == ' '
+			|| base[i] == '+' || base[i] == '-')
+			return (0);
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+
+int	check_begin(char *str, int *sign)
+{
+	int	i;
+	int	nb_sign;
+
+	i = 0;
+	nb_sign = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			nb_sign++;
+		i++;
+	}
+	if (nb_sign % 2 == 1)
+		*sign = -1;
+	else
+		*sign = 1;
+	return (i);
+}
+
+int	nb_base(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (c == base[i])
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	i;
+	int	j;
+	int	nb;
+	int	sign;
+	int	len;
+
+	len = check_base(base);
+	if (len < 2)
+		return (0);
+	i = check_begin(str, &sign);
+	j = nb_base(str[i], base);
+	while (j >= 0)
+	{
+		nb = nb * len + j;
+		i++;
+		j = nb_base(str[i], base);
+	}
+	return (nb * sign);
+}
--- C04/main.c
+++ C04/main.c
@@ -6,46 +6,51 @@
 /*   By: ndick <ndick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 16:50:10 by ndick             #+#    #+#             */
-/*   Updated: 2022/09/06 21:17:11 by ndick            ###   ########.fr       */
+/*   Updated: 2022/09/08 18:38:07 by ndick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
 //#include "ex00/ft_strlen.c"
-#include "ex01/ft_putstr.c"
+//#include "ex01/ft_putstr.c"
 //#include "ex02/ft_putnbr.c"
-#include "ex03/ft_atoi.c"
-#include "ex04/ft_putnbr_base.c"
+//#include "ex03/ft_atoi.c"
+//#include "ex04/ft_putnbr_base.c"
+#include "ex05/ft_atoi_base.c"
 
 int	main(void)
 {
-	char	str[] = "Hello World !";
+	//char	str[] = "Hello World !";
 	//int		nb = 123456789;
 	//int		neg_nb = -123456789;
-	char	str_to_int[] = " ---+--+1234ab567";
-	int		nbr_to_base = -424242;
-	char	base_bin[] = "01";
-	char	base_octale[] = "01234567" ;
-	char	base_hex[] = "0123456789ABCDEF";
-	char	base_custom[] = "/*987qwerty!~";
+	//char	str_to_int[] = " ---+--+1234ab567";
+	//int		nbr_to_base = -424242;
+	//char	base_bin[] = "01";
+	//char	base_octale[] = "01234567" ;
+	//char	base_hex[] = "0123456789ABCDEF";
+	//char	base_custom[] = "/*987qwerty!~";
 
 	//printf("%d\n", ft_strlen(str));
-	ft_putstr(str);
-	printf("\n");
+	//ft_putstr(str);
+	//printf("\n");
 	//ft_putnbr(nb);
 	//printf("\n");
 	//ft_putnbr(neg_nb);
 	//printf("\n");
-	printf("Result : %d",  ft_atoi(str_to_int));
-	printf("\n");
-	printf("Bin base :\n");
-	ft_putnbr_base(nbr_to_base, base_bin);
-	printf("\nOctale base :\n");
-	ft_putnbr_base(nbr_to_base, base_octale);
-	printf("\nHexa base :\n");
-	ft_putnbr_base(nbr_to_base, base_hex);
-	printf("\nCustom base :\n");
-	ft_putnbr_base(nbr_to_base, base_custom);
+	///printf("Result : %d",  ft_atoi(str_to_int));
+	//printf("\n");
+	//printf("Bin base :\n");
+	//ft_putnbr_base(nbr_to_base, base_bin);
+	//printf("\nOctale base :\n");
+	//ft_putnbr_base(nbr_to_base, base_octale);
+	//printf("\nHexa base :\n");
+	//ft_putnbr_base(nbr_to_base, base_hex);
+	//printf("\nCustom base :\n");
+	//ft_putnbr_base(nbr_to_base, base_custom);
+	//printf("\n");
+	printf("%d\n", ft_atoi_base("133742", "0123456789"));
+	printf("%d\n", ft_atoi_base("	     ---101010", "01"));
+	printf("%d\n", ft_atoi_base(" 	+---539", "0123456789abcdef"));
 	return (0);
 }
